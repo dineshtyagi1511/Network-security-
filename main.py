f651466500842dfd494365fd03201f41e5388f2d
dineshtyagi1511 <dineshtyagi15112002@gmail.com>
2026-07-25 01:06:45 +0530

    Data ingestion Completed

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from network-security!")
+from network_security.components.data_ingestion import DataIngestion
+from network_security.exception.exception import NetworkSecurityException
+from network_security.logging.logger import logging
+from network_security.entity.config_entity import DataIngestionConfig
+from network_security.entity.config_entity import TrainingPipelineConfig
+
+import sys 
+
+
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        trainingpipelineconfig=TrainingPipelineConfig()
+        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion = DataIngestion(dataingestionconfig)
+        logging.info("Initialize the data ingestion")
+        data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        print(data_ingestion_artifact)
+    except Exception as e:
+        logging.exception("An exception occurred")   # Logs full traceback
+        raise NetworkSecurityException(e, sys)
--- network_security/constants/training_pipeline/__init__.py
+++ network_security/constants/training_pipeline/__init__.py
@@ -8,11 +8,15 @@ PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
 FILE_NAME: str = "phisingData.csv"
 
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+
 """
 Data ingestion related constants start with DATA_INGESTION VAR NAME 
 """
 DATA_INGESTION_COLLECTION_NAME :str ="NetworkData"
-DATA_INGESTION_DATABASE_NAME:str ="DINESHTYAGI"
+DATA_INGESTION_DATABASE_NAME:str ="DINEShTYAGI"
 DATA_INGESTION_DIR_NAME:str ="data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
 DATA_INGESTION_INGESTED_DIR:str ="ingested"
